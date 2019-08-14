a9ce250e2f2aa725ec926bd04656b4c360fed7db
Ben Jenkins <ben.jenkins@icloud.com>
2019-08-14 16:08:39 -0500

    map has a route painted on it

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Walkers!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <View style={styles.container}>
+        <Text>Hello Walkers!</Text>
+        <MapView style={{ alignSelf: "stretch", height: 700 }}>
+          <Polyline
+            coordinates={[
+              { latitude: 41.895546, longitude: -87.639462 },
+              { latitude: 41.895498, longitude: -87.641509 },
+              { latitude: 41.893483, longitude: -87.641477 },
+              { latitude: 41.889138, longitude: -87.638076 },
+              { latitude: 41.889074, longitude: -87.640007 },
+              { latitude: 41.88394, longitude: -87.639782 }
+            ]}
+            strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+
+            strokeWidth={6}
+          />
+        </MapView>
+      </View>
+    </SafeAreaView>
   );
 }
 
