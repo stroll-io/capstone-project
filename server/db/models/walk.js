d8bb3513d9ce10aa3b8678ccd92e2d4490ff8542
Madison Carr <madisoncarr@Madisons-MacBook-Pro.local>
2019-08-15 15:47:29 -0500

    backend setup and initial routes

---
+++ server/db/models/walk.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Walk = db.define('walk', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.TEXT,
+    validate: {
+      len: [2, 600], //this may not work
+    },
+  },
+  category: {
+    type: Sequelize.ENUM,
+    values: [
+      'nature',
+      'scenic',
+      'architecture',
+      'dog',
+      'historical',
+      'hiking',
+      'street art',
+    ],
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+  },
+  stars: {
+    type: Sequelize.INTEGER,
+  },
+  userId: Sequelize.INTEGER,
+});
+
+module.exports = Walk;
