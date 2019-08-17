0db1eaaddc03bcb7481c1cddf98fc464065cd60b
Madison Carr <madisoncarr@Madisons-MacBook-Pro.local>
2019-08-17 18:19:38 -0500

    database updates with attractions and userpins

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,7 +1,8 @@
 const User = require('./user');
 const Walk = require('./walk');
 const NavPoint = require('./navPoint');
-
+const UserPin = require('./userPin');
+const Attraction = require('./attraction');
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -13,6 +14,12 @@ User.hasMany(Walk);
 
 NavPoint.belongsTo(Walk);
 Walk.hasMany(NavPoint);
+
+User.belongsToMany(Walk, { through: 'favorite_walks' });
+Walk.belongsToMany(User, { through: 'favorite_walks' });
+
+User.hasMany(UserPin);
+UserPin.belongsTo(User);
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -23,4 +30,6 @@ module.exports = {
   User,
   Walk,
   NavPoint,
+  UserPin,
+  Attraction,
 };
