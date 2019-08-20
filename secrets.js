bd24bdbb184af70ea9ebfe9ecf2b6594bb431101
Madison Carr <madison.carr@utexas.edu>
2019-08-20 14:07:05 -0500

    started on google directions

--- secrets.js
+++ secrets.js
@@ -1,3 +1,4 @@
 const ngrokSecret = 'http://47662a14.ngrok.io';
+const googleSecret = 'AIzaSyA-2WKG4KHUc4S-2TchjWjeNPqzZ9E9Iik';
 
-module.exports = ngrokSecret;
+module.exports = { ngrokSecret, googleSecret };
