b16f0202bc8394385e18f0b95f64e2e78409503c
madisoncarr <madison.carr@utexas.edu>
2019-08-24 15:39:24 -0500

    adds button to starred walks

--- secrets.js
+++ secrets.js
@@ -1,4 +1,4 @@
-const ngrokSecret = 'http://1900375b.ngrok.io';
+const ngrokSecret = 'http://986ad3a8.ngrok.io';
 const googleSecret = 'AIzaSyDD4BBjwuaT0NYpzsAQCALh4FXqz0tCP1A';
 
 module.exports = { ngrokSecret, googleSecret };
