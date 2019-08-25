603109506d3c420a83ec60fc71e968c32c398483
Michelle Hoang <michellehoang.inbox@gmail.com>
2019-08-24 19:39:16 -0500

    merging button backend

--- secrets.js
+++ secrets.js
@@ -1,4 +1,4 @@
-const ngrokSecret = 'http://afc89787.ngrok.io';
+const ngrokSecret = 'http://1ff5717f.ngrok.io';
 const googleSecret = 'AIzaSyDD4BBjwuaT0NYpzsAQCALh4FXqz0tCP1A';
 
 module.exports = { ngrokSecret, googleSecret };
