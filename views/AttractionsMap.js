964d076304ac8259fc8398cb4360b037b68e651b
madisoncarr <madison.carr@utexas.edu>
2019-08-27 14:40:49 -0500

    adds the modal information to the nearby attractions

--- views/AttractionsMap.js
+++ views/AttractionsMap.js
@@ -3,7 +3,7 @@ import { View, SafeAreaView, Modal, Text } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { connect } from 'react-redux';
 import { Form, Item, Picker, Icon, Button } from 'native-base';
-import { AntDesign } from 'react-native-vector-icons';
+import { AntDesign, SimpleLineIcons } from 'react-native-vector-icons';
 import {
   getAllAttractionsThunk,
   getAttractionsByTagThunk,
@@ -22,26 +22,6 @@ function AttractionsMap(props) {
   const closeModal = () => {
     setIsModalVisible(false);
   };
-  // const handleBack = () => {
-  //   setIsModalVisible(false);
-  //   setTitle('');
-  //   setDescription('');
-  //   setIsPinBeingAdded(false);
-  //   setCoord(null);
-  // };
-
-  // const handleSubmit = async () => {
-  //   props.addPin({
-  //     coordinate: coord,
-  //     title,
-  //     description,
-  //   });
-  //   setIsPinBeingAdded(false);
-  //   setIsModalVisible(false);
-  //   setCoord(null);
-  //   setTitle('');
-  //   setDescription('');
-  // };
 
   // let filter = 'Filter by tag';
   const handlePicker = e => {
@@ -82,8 +62,89 @@ function AttractionsMap(props) {
                 marginTop: 50,
               }}
             >
-              Here is some text in the modal
+              Filter by tag to see the types of attractions nearby.
             </Text>
+            <View
+              style={{ display: 'flex', flexDirection: 'row', marginTop: 10 }}
+            >
+              <SimpleLineIcons
+                name="location-pin"
+                size={25}
+                color="#478FCD"
+                style={{ padding: 5 }}
+              />
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontFamily: 'Avenir-Heavy',
+                  paddingTop: 5,
+                }}
+              >
+                Architecture
+              </Text>
+            </View>
+            <View
+              style={{ display: 'flex', flexDirection: 'row', marginTop: 10 }}
+            >
+              <SimpleLineIcons
+                name="location-pin"
+                size={25}
+                color="#5fAD46"
+                style={{ padding: 5 }}
+              />
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontFamily: 'Avenir-Heavy',
+                  paddingTop: 5,
+                }}
+              >
+                Nature
+              </Text>
+            </View>
+            <View
+              style={{ display: 'flex', flexDirection: 'row', marginTop: 10 }}
+            >
+              <SimpleLineIcons
+                name="location-pin"
+                size={25}
+                color="violet"
+                style={{ padding: 5 }}
+              />
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontFamily: 'Avenir-Heavy',
+                  paddingTop: 5,
+                }}
+              >
+                Art and Museums
+              </Text>
+            </View>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                marginTop: 10,
+                marginBottom: 20,
+              }}
+            >
+              <SimpleLineIcons
+                name="location-pin"
+                size={25}
+                color="tomato"
+                style={{ padding: 5 }}
+              />
+              <Text
+                style={{
+                  fontSize: 18,
+                  fontFamily: 'Avenir-Heavy',
+                  paddingTop: 5,
+                }}
+              >
+                Historical
+              </Text>
+            </View>
           </View>
           <View style={{ justifyContent: 'center' }}>
             <Button
