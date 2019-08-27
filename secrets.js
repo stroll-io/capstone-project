48339e0ac280666ccf92a3414d6cb87fcb4ca0d4
madisoncarr <madison.carr@utexas.edu>
2019-08-27 16:18:53 -0500

    ngrok stuff

--- secrets.js
+++ secrets.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const ngrokSecret = 'http://3729ea25.ngrok.io';
-=======
-const ngrokSecret = 'http://b07c3b29.ngrok.io';
->>>>>>> d19b0b0dbf91f540abf8754afb58402f826f5c2b
+const ngrokSecret = 'http://c0e3d49a.ngrok.io';
 const googleSecret = 'AIzaSyDD4BBjwuaT0NYpzsAQCALh4FXqz0tCP1A';
 
 module.exports = { ngrokSecret, googleSecret };
